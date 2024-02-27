afd914d5db05f93f08192c5d53ff4cbeb2445e8e
Hamza Shameem <hamza.shameem@keype.com>
2024-02-27 22:56:16 +0530

    Initial Commit

---
+++ ims/src/main/java/com/hsd/ims/model/Institute.java
@@ -0,0 +1,26 @@
+package com.hsd.ims.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Institute {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String location;
+	private String contact;
+	
+	
+	
+}
---
+++ ims/src/main/java/com/hsd/ims/service/InstituteService.java
@@ -0,0 +1,14 @@
+package com.hsd.ims.service;
+
+import org.springframework.stereotype.Service;
+
+import com.hsd.ims.model.Institute;
+
+@Service
+public class InstituteService {
+	
+	public void createInstitute() {
+		Institute institute = new Institute();
+//		institute.s
+	}
+}
